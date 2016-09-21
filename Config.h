2d459cc597eb3eeb2945c29124071a8a1c6de181
Christoph Wagner <chrilli@chrillomat.net>
2016-09-21 23:36:21 +0200

    adapt to RAMPSv1.4 based hardware together with LXD75 mount

--- Config.h
+++ Config.h
@@ -110,19 +110,19 @@
                                      // for the most part this doesn't need to be changed, but adjust when needed.  Default=25
 
                                      // Axis1 is for RA/Az
-#define StepsPerDegreeAxis1  14400.0 // calculated as    :  stepper_steps * micro_steps * gear_reduction1 * (gear_reduction2/360)
+#define StepsPerDegreeAxis1  3200.0 // calculated as    :  stepper_steps * micro_steps * gear_reduction1 * (gear_reduction2/360)
                                      // Takahashi EM10b  :  200           * 8           * 18 * (1.25)     *  144/360              = 14400
                                      // Axis2 is for Dec/Alt
-#define StepsPerDegreeAxis2  14400.0 // calculated as    :  stepper_steps * micro_steps * gear_reduction1 * (gear_reduction2/360)
+#define StepsPerDegreeAxis2  3200.0 // calculated as    :  stepper_steps * micro_steps * gear_reduction1 * (gear_reduction2/360)
                                      // Takahashi EM10b  :  200           * 8           * 18 * (1.25)     *  144/360              = 14400
                                      // the EM10b has two spur gears that drive the RA/Dec worms, they give an additional 1.25:1 reduction
                                      // in addition to the 18:1 gear heads on the steppers for a 22.5:1 final ratio before the worm/wheels at 144:1
                                      
                                      // PEC, number of steps for a complete worm rotation (in RA), (StepsPerDegreeAxis1*360)/gear_reduction2.  Ignored on Alt/Azm mounts.
-#define StepsPerWormRotationAxis1 36000
+#define StepsPerWormRotationAxis1 8000
                                      // Tak EM10         : (14400*360)/144 = 36000
 
-#define PECBufferSize           824  // PEC, buffer size, max should be no more than 3384, your required buffer size >= StepsPerAxis1WormRotation/(StepsPerDegeeAxis1/240)
+#define PECBufferSize           600  // PEC, buffer size, max should be no more than 3384, your required buffer size >= StepsPerAxis1WormRotation/(StepsPerDegeeAxis1/240)
                                      // for the most part this doesn't need to be changed, but adjust when needed.  824 seconds is the default.  Ignored on Alt/Azm mounts.
 
 #define REVERSE_AXIS1_OFF            // reverse the direction of movement for the HA/RA axis, adjust as needed or reverse your wiring so things move in the right direction
@@ -165,12 +165,12 @@
 // open,VCC_IO     4 (Quarterstep)  Y, to 256 μsteps   stealthChop     0b110001
 // open,open      16 μsteps         Y, to 256 μsteps       "           0b111000
 */
-#define AXIS1_MODE_OFF               // programs the RA/Az uStep mode M0/M1/M2, optional and default _OFF.
-#define AXIS1_MODE_GOTO_OFF          // programs the RA/Az uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
-#define AXIS1_STEP_GOTO 1            // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
-#define AXIS2_MODE_OFF               // programs the Dec/Alt uStep mode M0/M1/M2, optional and default _OFF.
-#define AXIS2_MODE_GOTO_OFF          // programs the Dec/Alt uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
-#define AXIS2_STEP_GOTO 1            // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
+#define AXIS1_MODE 7                 // programs the RA/Az uStep mode M0/M1/M2, optional and default _OFF.
+#define AXIS1_MODE_GOTO 1            // programs the RA/Az uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
+#define AXIS1_STEP_GOTO 8             // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
+#define AXIS2_MODE 7                 // programs the Dec/Alt uStep mode M0/M1/M2, optional and default _OFF.
+#define AXIS2_MODE_GOTO 1            // programs the Dec/Alt uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
+#define AXIS2_STEP_GOTO 8            // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
 #define MODE_SWITCH_BEFORE_SLEW_OFF  // _ON for _MODE and _MODE_GOTO settings to start/stop just before/after the slew, otherwise they are active during the slew at <128uS/step)
 
 // Secondary stepper driver decay control (for both Axis, the _DECAY_MODE and _DECAY_MODE_GOTO settings always start/stop just before/after the slew)
